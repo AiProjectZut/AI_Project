133d0de5a56a798c41071862a845a70d639d17e3
MikolajRoj <150631447+MikolajRoj@users.noreply.github.com>
2024-01-21 21:21:40 +0100

    Add files via upload

--- Public/main.js
+++ Public/main.js
@@ -2,7 +2,70 @@ let startTime;
 let interval;
 const userInputField = document.getElementById('userInput');
 const textToTypeField = document.getElementById('textToType');
-const originalText = textToTypeField.textContent;
+let originalText;
+
+// New snippet to select a random sentence from an array
+    const sentences = [
+        'Nullam pulvinar vitae mi quis volutpat.',
+        'Integer imperdiet dolor vitae ultricies malesuada.',
+        'Quisque bibendum eu urna sed convallis.',
+        'Maecenas malesuada velit ut nulla consequat, in rutrum nulla bibendum.',
+        'Nulla quis magna et dui eleifend sodales.',
+        'Proin a pretium augue, non scelerisque quam.',
+        'Aliquam tincidunt massa est, dapibus semper ante viverra non.',
+        'Donec orci ligula, blandit eget pretium in, luctus ac sem.',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        'Vivamus quis tortor feugiat, commodo diam sit amet, blandit augue.'
+    ];
+
+    function getRandomSentence() {
+        const randomIndex = Math.floor(Math.random() * sentences.length);
+        return sentences[randomIndex];
+    }
+    
+    function initializeNewTypingTest() {
+        originalText = getRandomSentence();
+        textToTypeField.innerHTML = originalText;
+        userInputField.value = '';
+        clearInterval(interval);
+        startTime = undefined;
+        // Any other reset operations
+    }
+    
+    document.addEventListener('DOMContentLoaded', (event) => {
+        initializeNewTypingTest();
+    originalText = getRandomSentence();
+    textToTypeField.textContent = originalText;
+});
+
+document.addEventListener('keydown', (event) => {
+    let keyId;
+    if (event.code === 'Space') {
+        keyId = 'keySpace';
+    } else {
+        keyId = `key${event.key.toUpperCase()}`;
+    }
+
+    const keyElement = document.getElementById(keyId);
+    if (keyElement) {
+        keyElement.classList.add('active');
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    let keyId;
+    if (event.code === 'Space') {
+        keyId = 'keySpace';
+    } else {
+        keyId = `key${event.key.toUpperCase()}`;
+    }
+
+    const keyElement = document.getElementById(keyId);
+    if (keyElement) {
+        keyElement.classList.remove('active');
+    }
+});
+
 
 userInputField.addEventListener('input', function() {
     // Restart the timer if it's a new session
@@ -10,7 +73,11 @@ userInputField.addEventListener('input', function() {
         startTime = new Date();
         interval = setInterval(updateTime, 1000);
     }
-
+    
+    if (this.value === originalText) {
+        initializeNewTypingTest();
+        // Restart the timer if necessary
+    }
     let correctChars = 0;
     let isTypingCorrect = true;
 
